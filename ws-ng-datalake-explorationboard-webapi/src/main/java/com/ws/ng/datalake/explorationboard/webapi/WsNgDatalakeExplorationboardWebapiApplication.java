872dcc90a1cdf893f0f98ff0da5edafa2841498a
yacovyi@rnd-hub.com <yacovyi@rnd-hub.com>
2019-12-23 16:37:24 +0200

    add loginfra and Dockerfile

--- ws-ng-datalake-explorationboard-webapi/src/main/java/com/ws/ng/datalake/explorationboard/webapi/WsNgDatalakeExplorationboardWebapiApplication.java
+++ ws-ng-datalake-explorationboard-webapi/src/main/java/com/ws/ng/datalake/explorationboard/webapi/WsNgDatalakeExplorationboardWebapiApplication.java
@@ -2,8 +2,16 @@ package com.ws.ng.datalake.explorationboard.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+        basePackages = {
+                "com.ws.ng.loginfra.web"},
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*Test.*")}
+)
 public class WsNgDatalakeExplorationboardWebapiApplication {
 
     public static void main(String[] args) {
