5af4aef3b503993f621c3bf147ced90718be3369
yacovyi@rnd-hub.com <yacovyi@rnd-hub.com>
2019-12-23 16:15:19 +0200

    first commit

---
+++ ws-ng-datalake-explorationboard-webapi/src/main/java/com/ws/ng/datalake/explorationboard/webapi/WsNgDatalakeExplorationboardWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.ws.ng.datalake.explorationboard.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsNgDatalakeExplorationboardWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsNgDatalakeExplorationboardWebapiApplication.class, args);
+    }
+
+}
---
+++ ws-ng-datalake-explorationboard-webapi/src/test/java/com/ws/ng/datalake/explorationboard/webapi/WsNgDatalakeExplorationboardWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.ng.datalake.explorationboard.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsNgDatalakeExplorationboardWebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
